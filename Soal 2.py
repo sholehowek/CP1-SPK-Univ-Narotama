cae8651dca8b1b400ba7fbe4d5f849d71a8dc3fb
sholeh <sholeh313@gmail.com>
2018-10-09 21:01:38 +0700

    Penyelesaian tugas separuh

---
+++ Soal 2.py
@@ -0,0 +1,12 @@
+arrjalan = [
+		{"dari":"0", "ke":"1", "jarak": 13},
+		{"dari":"0", "ke":"2", "jarak": 4},
+		{"dari":"1", "ke":"3", "jarak": 6,
+		{"dari":"1", "ke":"2", "jarak": 2},
+		{"dari":"2", "ke":"4", "jarak": 1},
+		{"dari":"3", "ke":"5", "jarak": 5},
+		{"dari":"4", "ke":"3", "jarak": 5},
+		{"dari":"4", "ke":"5", "jarak": 13},
+]
+
+arrjalan = 
